50a77830f31e81b08bda1a005228189e652c90ff
DanyQ89 <108182297+DanyQ89@users.noreply.github.com>
2024-03-29 15:35:27 +0300

    big meow
    commit before release on server

--- Test_2.py
+++ Test_2.py
@@ -1,4 +1,5 @@
 import hmac
+import pprint
 import time
 from hashlib import sha256
 
@@ -9,47 +10,84 @@ APIKEY = "0AmO4AG3tJUhRKEcnM7UTwVSeZ3XQyWnTNQSDhGfKyn6D6yp9FKHLs8wz2iEhVUNswyraw
 SECRETKEY = "ukvwXsBH4tzk2VCB9mVuuI83MtNOTIdlymfKRQUeKnEwNGdtegJx7HgIgkOg88Blb08LOjhNLFQO9ySqaQbw"
 
 
-# def demo(symbol, side, positionSide, value, leverage, margin):
-def demo():
+def demo(symbol, side, positionSide, leverage, quantity):
+    # def demo():
     payload = {}
     path = '/openApi/swap/v2/trade/order'
     method = "POST"
-    # paramsMap = {
-    #     "symbol": symbol,
-    #     "side": side, #buy-sell
-    #     "positionSide": positionSide, #long-short
-    #     "type": "MARKET",
-    #     "quantity": value,
-    #     'leverage': leverage,
-    #     'margin': margin
-    # }
     paramsMap = {
-        'side': 'BUY',
-        'positionSide': 'LONG',
-        'symbol': 'XRP-USDT',
-        'leverage': 5,
-        'margin': 2,
-        'type': 'MARKET'
-
+        "symbol": symbol,
+        "side": side,  # buy-sell
+        "positionSide": positionSide,  # long-short
+        "type": "MARKET",
+        'leverage': leverage,
+        'quantity': quantity
     }
+    # paramsMap = {
+    #     'side': 'BUY',
+    #     'positionSide': 'LONG',
+    #     'symbol': 'XRP-USDT',
+    #     'leverage': 5,
+    #     'margin': 2,
+    #     'type': 'MARKET',
+    #     'workingType': 'MARK_PRICE'
+    #
+    # }
     paramsStr = parseParam(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
 
+def get_balance():
+    payload = {}
+    path = '/openApi/swap/v2/user/balance'
+    method = "GET"
+    paramsMap_balance = {
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr_balance = parseParam(paramsMap_balance)
+    return send_request(method, path, paramsStr_balance, payload)
+
+
+def get_price(coin):
+    payload = {}
+    path = '/openApi/swap/v2/quote/ticker'
+    method = "GET"
+    paramsMap_balance = {
+        "timestamp": str(int(time.time() * 1000)),
+        'symbol': coin
+    }
+
+    paramsStr_balance = parseParam(paramsMap_balance)
+    return send_request(method, path, paramsStr_balance, payload)
+
+
+def get_min_price():
+    payload = {}
+    path = '/openApi/swap/v2/quote/contracts'
+    method = "GET"
+    paramsMap_min_price = {
+        "timestamp": str(int(time.time() * 1000))
+    }
+
+    paramsStr_min_price = parseParam(paramsMap_min_price)
+    return send_request(method, path, paramsStr_min_price, payload)
+
+
 def get_sign(api_secret, payload):
     signature = hmac.new(api_secret.encode("utf-8"), payload.encode("utf-8"), digestmod=sha256).hexdigest()
-    print("sign=" + signature)
+    # print("sign=" + signature)
     return signature
 
 
 def send_request(method, path, urlpa, payload):
     url = "%s%s?%s&signature=%s" % (APIURL, path, urlpa, get_sign(SECRETKEY, urlpa))
-    print(url)
+    # print(url)
     headers = {
         'X-BX-APIKEY': APIKEY,
     }
-    response = requests.request(method, url, headers=headers, data=payload)
-    return response.text
+    response = requests.request(method, url, headers=headers, data=payload).json()
+
+    return response
 
 
 def parseParam(paramsMap):
@@ -60,13 +98,16 @@ def parseParam(paramsMap):
     else:
         return paramsStr + "timestamp=" + str(int(time.time() * 1000))
 
+# pprint.pprint(get_price('1000PEPE-USDT'))
+# pprint.pprint(get_price('SAND-USDT'))
+#
+# import pprint
+# pprint.pprint(get_price('BTC-USDT'))
 
-# if __name__ == '__main__':
-#     print("demo:", demo())
+# for i in get_min_price()['data']:
+#     if i['maxLongLeverage'] > 10:
+#         pprint.pprint(i)
 
-try:
-    a = demo()
-    print(a)
-    print('sucess')
-except Exception as err:
-    print(err)
+# for i in get_min_price()['data']:
+#     if i['symbol'] == 'SAND-USDT':
+#         pprint.pprint(i)
--- Work2.py
+++ Work2.py
@@ -9,11 +9,15 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardRemove, Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-from Test_2 import demo
+from Test_2 import demo, get_balance, get_price, get_min_price
 
 API_TOKEN = '7057397327:AAEhF8YpnrJTYPUo58RGTXDT3ZbHIaoj1J4'
 
 form_router = Router()
+
+builder_home = ReplyKeyboardBuilder()
+builder_home.add(types.KeyboardButton(text='Домой'))
+builder_home_ready = builder_home.as_markup(resize_keyboard=True)
 
 
 class Form(StatesGroup):
@@ -23,19 +27,28 @@ class Form(StatesGroup):
     value = State()
     leverage = State()
     margin = State()
+    balance = 0
+    price = 0
+    lev_data = []
 
 
 @form_router.message(Command('start'))
 @form_router.message(F.text == 'Домой')
 async def cmd_start(message: Message, state: FSMContext):
     await state.clear()
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='BUY'))
-    builder.add(types.KeyboardButton(text="SELL"))
-    builder.add(types.KeyboardButton(text="Домой"))
-    builder.adjust(2)
-    await state.set_state(Form.operation)
-    await message.answer('Выберите операцию', reply_markup=builder.as_markup(resize_keyboard=True))
+    balance = float(get_balance()['data']['balance']['balance'])
+    if not balance:
+        await message.answer('Ваш баланс равен 0, пополните баланс и попробуйте снова',
+                             reply_markup=builder_home_ready)
+    else:
+        Form.balance = balance
+        builder = ReplyKeyboardBuilder()
+        builder.add(types.KeyboardButton(text='BUY'))
+        builder.add(types.KeyboardButton(text="SELL"))
+        builder.add(types.KeyboardButton(text="Домой"))
+        builder.adjust(2)
+        await state.set_state(Form.operation)
+        await message.answer('Выберите операцию', reply_markup=builder.as_markup(resize_keyboard=True))
 
 
 @form_router.message(Form.operation)
@@ -57,40 +70,128 @@ async def crypto_s(message: Message, state: FSMContext):
     await message.answer('Введите название валюты', reply_markup=ReplyKeyboardRemove())
 
 
+# @form_router.message(Form.symbol)
+# async def crypto_n(message: Message, state: FSMContext):
+#     await state.update_data(symbol=f'{message.text.upper()}-USDT')
+#     await state.set_state(Form.value)
+#     await message.answer('Введите количество монет в формате 1.0 или 0.01', reply_markup=ReplyKeyboardRemove())
+
 @form_router.message(Form.symbol)
-async def crypto_n(message: Message, state: FSMContext):
-    await state.update_data(symbol=f'{message.text.upper()}-USDT')
-    await state.set_state(Form.value)
-    await message.answer('Введите количество монет в формате 1.0 или 0.01', reply_markup=ReplyKeyboardRemove())
-
-
-@form_router.message(Form.value)
-async def value(message: Message, state: FSMContext):
-    await state.update_data(value=float(message.text))
-    await state.set_state(Form.leverage)
-    await message.answer('Введите плечо или 0', reply_markup=ReplyKeyboardRemove())
-
-
-@form_router.message(Form.leverage)
 async def lev(message: Message, state: FSMContext):
-    await state.update_data(leverage=int(message.text))
+    do = False
+    while True:
+        if do:
+            await state.set_state(Form.symbol)
+            msg = Message.text.upper()
+        else:
+            msg = message.text.upper()
+        try:
+            need = msg + '-USDT'
+            price = float(get_price(need)['data']['bidPrice'])
+            if price:
+                await state.update_data(symbol=need)
+                break
+            else:
+                await message.answer('Цена монеты равна 0.0, введите другую монету',
+                                     reply_markup=builder_home_ready)
+                do = True
+        except Exception:
+            await message.answer('Монета не найдена, убедитесь в правильности названия монеты',
+                                 reply_markup=builder_home_ready)
+            do = True
+
     await state.set_state(Form.margin)
-    await message.answer('Введите маржу в процентах', reply_markup=ReplyKeyboardRemove())
+
+    balance = Form.balance
+    Form.price = price
+    value_second = 0
+    value_symb = 0
+
+    for i in get_min_price()['data']:
+        if i['symbol'] == need:
+            value_symb = i['tradeMinUSDT']
+            value_second = i['tradeMinQuantity']
+            Form.lev_data = [i['maxLongLeverage'], i['maxShortLeverage']]
+
+    m_second = round(price / balance * value_second * 100, 2)
+    m_first = round(value_symb / balance, 2) * 100
+    # minn = min(m_first, m_second)
+    maxx = max(m_first, m_second)
+    await message.answer(
+        f'На вашем счету доступно {balance}\nЦена монеты: {price}'
+        f'\nМинимальная маржа: {maxx}%\nВведите маржу в процентах',
+        reply_markup=ReplyKeyboardRemove())
 
 
 @form_router.message(Form.margin)
-async def crypto_all(message: Message, state: FSMContext):
-    data = await state.update_data(margin=int(message.text))
-    await state.clear()
-    print(data)
+async def value(message: Message, state: FSMContext):
+    do = False
+    while True:
+        if do:
+            a = Message.text
+        else:
+            a = message.text
+        try:
+            a = int(a)
+            if 0 < a <= 100:
+                await state.update_data(margin=a)
+                break
+            else:
+                await message.answer('Введите маржу от 0% до 100%')
+                do = True
+        except Exception:
+            await message.answer('Маржа должна быть числом')
+            do = True
 
+    await state.set_state(Form.leverage)
+    # print(Form.lev_data)
+    lev_max = Form.lev_data[0] if Form.operation == 'LONG' else Form.lev_data[1]
+    await message.answer(f'Введите плечо от 0 до {lev_max}', reply_markup=ReplyKeyboardRemove())
+
+
+@form_router.message(Form.leverage)
+async def crypto_all(message: Message, state: FSMContext):
+    lev_max = Form.lev_data[0] if Form.operation == 'LONG' else Form.lev_data[1]
+    do = False
+    while True:
+        if do:
+            lev_real = Message.text
+        else:
+            lev_real = message.text
+        try:
+            lev_real = int(lev_real)
+            if 0 <= lev_real <= lev_max:
+                data = await state.update_data(leverage=lev_real)
+                break
+            else:
+                await message.answer(f'Введите плечо в диапазоне от 0 до {lev_max}')
+                do = True
+        except Exception:
+            await message.answer('Плечо должно быть числом')
+            do = True
+
+    await state.clear()
+
+    # print(data)
+
+    quantity = (Form.balance * (data['margin'] / 100)) / Form.price
     try:
 
-        demo(data["symbol"], data["operation"], data["sl"], data["value"], data["leverage"], data["margin"])
-        await message.answer('Операция выполнена успешно')
+        a = demo(data["symbol"], data["operation"], data["sl"], data["leverage"], quantity)
+        msg = a['msg']
+        # print(a)
+        if not msg or not a['success'] == 'false':
+            await message.answer('Операция выполнена успешно', reply_markup=builder_home_ready)
+
+        else:
+            await message.answer(f'Ошибка при выполнении операции\n{msg}\n{a["code"]}', reply_markup=builder_home_ready)
+
     except Exception as err:
-        await message.answer(f'Ошибка при выполнении операции\n{err}')
-        print(err)
+        await message.answer(
+            f'Ошибка при выполнении операции\n{err}\nВаши данные:\n{", ".join([str(i) for i in data.values()])}',
+            reply_markup=builder_home_ready)
+
+        # print(err)
 
 
 async def main():
